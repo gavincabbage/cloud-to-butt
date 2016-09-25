af3782ec909c9e24a0880857f90c132f5ad1ca54
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-09-24 23:45:42 -0400

    updated rules based on first feedback

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 const RULES = [
-	{ regex: /\bSchalke\b/g, replacement: 'Shitke' },
-	{ regex: /\bschalke\b/g, replacement: 'shitke' },
-	{ regex: /\bSchalke04\b/g, replacement: 'Shitke04' },
-	{ regex: /\bschalke04\b/g, replacement: 'shitke04' },
-	{ regex: /\bS04\b/g, replacement: 'Shit04' },
-	{ regex: /\bs04\b/g, replacement: 'shit04' },
-	{ regex: /blauen/g, replacement: 'scheissen' },
-	{ regex: /\bBlauen\b/g, replacement: 'Scheissen' },
-	{ regex: /\bMiners\b/g, replacement: 'Shitters' },
-	{ regex: /\bminers\b/g, replacement: 'shitters' },
+	{ regex: /\b[Ff][Cc] [Ss]chalke 04\b/g, replacement: 'FC Coleslaw 04' },
+	{ regex: /\b[Ss]chalke\b/g, replacement: 'Shitke' },
+	{ regex: /\b[Ss]chalke04\b/g, replacement: 'Shitke04' },
+	{ regex: /\b[Ss]04\b/g, replacement: '03+1' },
+	{ regex: /\b[Kk]önigsblauen/g, replacement: 'Königscheissen' },
 	{ regex: /\bSCHALKE\b/g, replacement: 'SHITKE' },
-	{ regex: /\bBlues\b/g, replacement: 'Shits' },
-	{ regex: /\bblues\b/g, replacement: 'shits' },
-	{ regex: /\bGelsenkirchen\b/g, replacement: 'Herne West' },
-	{ regex: /\bgelsenkirchen\b/g, replacement: 'herne west' },
+	{ regex: /\b[Gg]elsenkirchen\b/g, replacement: 'Herne West' },
+	{ regex: /\b[Rr]oyal [Bb]lues\b/g, replacement: 'Royal Shits' },
+	{ regex: /\b[Ee]mbolo\b/g, replacement: 'Ebola Plunger' },
+	{ regex: /\b[Hh]untelaar\b/g, replacement: 'Satan' },
 ];
 
 function walk(node)
